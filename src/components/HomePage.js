32968870cb174c968df037cd05f8cafcb986c2fc
Logan Sutherland <loganrsutherland@gmail.com>
2021-04-03 17:43:54 -0400

    style: add icons

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -17,6 +17,18 @@ const HomePage = (props) => {
         alertLocations={props.alertLocations}
         types={props.types}
       />
+      <div id="copyright">
+        <div>
+          Icon made by{' '}
+          <a href="https://www.freepik.com" title="Freepik">
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

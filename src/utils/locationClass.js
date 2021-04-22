99f60cf197d1dfd9ff3caaf74d41804e47df3077
Logan Sutherland <loganrsutherland@gmail.com>
2021-04-22 12:29:59 -0400

    docs: remove documentation for location class

--- src/utils/locationClass.js
+++ src/utils/locationClass.js
@@ -1,25 +1,3 @@
-// class VaxLocation {
-//   constructor(
-//     updatedAt,
-//     name,
-//     address,
-//     link,
-//     type,
-//     wheelchairAccess,
-//     appointments,
-//     notes
-//   ) {
-//     this.updatedAt = updatedAt;
-//     this.name = name;
-//     this.address = address;
-//     this.link = link;
-//     this.type = type;
-//     this.wheelchairAccess = wheelchairAccess;
-//     this.appointments = appointments;
-//     this.notes = notes;
-//   }
-// }
-
 class VaxLocation {
   constructor(updatedAt, name, address, appointments, notes, link, types) {
     this.updatedAt = updatedAt;
